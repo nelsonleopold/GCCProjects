389301118414039d1f5f6b2d67f2490936519a01
nelsonleopold <nelsonleopoldiv@guamcc.edu>
2020-09-02 08:48:08 +1000

    This is my first commit

---
+++ FLastName_age.py
@@ -0,0 +1,10 @@
+#Name: Nelson Leopold
+#Date: 8/31/2020
+#Describe what the program will do -- The following code will calculate how old you will be in 10 years and what year you will be 100.
+
+myFullName = input("Please input your full name: ")
+ageNow = input("Please enter your current age: ")
+
+print("My name is " + myFullName + ".")
+print("In 10 years I will be " + str(10 + int(ageNow)) + " years old.")
+print("In the year " + str(100 - int(ageNow) + 2020) + ", I will be 100 years old.")
