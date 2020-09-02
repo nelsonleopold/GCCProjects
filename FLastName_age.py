d6b7010f8fab9ec3c70082a34f9cefa8be9c4a8a
nelsonleopold <nelsonleopoldiv@guamcc.edu>
2020-09-02 11:14:37 +1000

    imported datetime, changed year calc

--- FLastName_age.py
+++ FLastName_age.py
@@ -2,9 +2,12 @@
 #Date: 8/31/2020
 #Describe what the program will do -- The following code will calculate how old you will be in 10 years and what year you will be 100.
 
+import datetime
+
+currentDate = datetime.datetime.now()
 myFullName = input("Please input your full name: ")
 ageNow = input("Please enter your current age: ")
 
 print("My name is " + myFullName + ".")
 print("In 10 years I will be " + str(10 + int(ageNow)) + " years old.")
-print("In the year " + str(100 - int(ageNow) + 2020) + ", I will be 100 years old.")
+print("In the year " + str(100 - int(ageNow) + currentDate.year) + ", I will be 100 years old.")
